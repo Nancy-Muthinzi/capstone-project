5c01109abc33f29946135bdada64b90720fe00e6
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-10-24 11:28:33 +0300

    create blog and category models

--- shop/admin.py
+++ shop/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Image, Profile, Blog, Tag
+from .models import Image, Profile, Blog, Category
 
 admin.site.register(Image)
 admin.site.register(Profile)
 admin.site.register(Blog)
-admin.site.register(Tag)
+admin.site.register(Category)
--- shop/models.py
+++ shop/models.py
@@ -6,26 +6,55 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, default=True)
     names = models.CharField(max_length=50)
     email = models.EmailField()
-        
+
     def __str__(self):
         return self.names
 
     class Meta:
         ordering = ['names']
 
-class Tag (models.Model):
-    name = models.CharField(max_length =30)
-
-    def __str__(self):
-        return self.name        
 
 class Image(models.Model):
     id = models.AutoField(primary_key=True)
     image = models.ImageField(upload_to='home/', blank=True)
-    name = models.CharField(max_length=25, blank=True)
-    caption = models.TextField(max_length=25)
-    tag = models.ManyToManyField(Tag)
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, blank=True)
+    description = models.TextField(max_length=75)
+    category = models.ManyToManyField('Category')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def retrive_all_images(cls):
+        images = Image.objects.all()
+        return images
+
+    @classmethod
+    def get_image_by_id(cls, id):
+        images = cls.objects.get(pk=id)
+        return images
+
+class Category(models.Model):
+    name = models.CharField(max_length =25)
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()    
+
+    def __str__(self):
+        return self.name
+   
 
 class Blog(models.Model):
     author = models.ForeignKey(User)
