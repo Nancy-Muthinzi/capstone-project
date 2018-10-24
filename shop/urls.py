f7d3f56b2d56ab7b820853576b6549167aa3126c
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-10-24 12:35:42 +0300

    add search bar, function & url

--- shop/urls.py
+++ shop/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
     url('^$',views.home,name = 'home'),
-    url('^today/$',views.home,name='siteToday')
+    url('^today/$',views.home,name='siteToday'),
+    url(r'^search/', views.search_results, name='search_results')
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import datetime as dt
+from .models import Image
+
 
 def home(request):
     date = dt.date.today()
-    return render(request, 'home.html', {'date':date})
+    images = Image.objects.all()
+
+    return render(request, 'home.html', {'date':date, 'images':images})
+
+def shop(request):
+
+    return render(request, 'shop.html')
+
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_category(search_term)
+
+        message = f"{search_term}"
+        return render(request, 'search.html', {"message":message, "images": searched_images})
+
+    else:
+        message = "You haven't made any searches"
+        return render(request, 'search.html', {"message":message})
