5a84fef7eb7d481a1f147525fb66b2cc63e46374
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-10-25 22:39:04 +0300

    add django cart

--- capstone/settings.py
+++ capstone/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'shop.apps.ShopConfig',
     'bootstrap4',
     'djmoney',
+    'cart',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
